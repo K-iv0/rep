d9795f2bb362491a7efbe59711b7aae14f0f0c7d
ksema <ksemadilema80@gmail.com>
2024-08-25 15:18:43 +0500

    27.08.24

---
+++ maiy.py
@@ -0,0 +1,10 @@
+# "1st program"
+print(9**0.5*5)
+# "2hd program"
+print(99.9>99.8<1000!=1000.1)
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+# "4th program"
+print(float((123.456)*10)%10)
